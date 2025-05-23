d6f759861cd7a35d3157b4a4c2f00ea4cc53bde2
Al Macey <al_macey@hotmail.com>
2025-05-22 20:34:10 -0400

    Updated home.html to reflect a sample telco customer site and added a new account.html page to show the customer account details.

--- app.py
+++ app.py
@@ -9,12 +9,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    print('Request for index page received')
-   return render_template('index.html')
+   return render_template('home.html')
 
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/account')
+def account():
+    user = {
+        'name': 'Taylor Morgan',
+        'account_number': 'TELCO-2025001',
+        'email': 'taylor.morgan@mytelco.com',
+        'phone': '+1 (555) 246-8100',
+        'plan': 'Premium Unlimited',
+        'balance': '$15.75',
+        'status': 'Active',
+        'address': '789 Oak Avenue, Gotham, USA'
+    }
+    return render_template('account.html', user=user)
 
 @app.route('/hello', methods=['POST'])
 def hello():
@@ -30,3 +44,8 @@ def hello():
 
 if __name__ == '__main__':
    app.run()
+
+
+
+
+
